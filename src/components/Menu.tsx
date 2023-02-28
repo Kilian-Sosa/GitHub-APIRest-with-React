b99763dfcdb731e5e97eb6d19f3780f9f5fbd0a2
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-02-28 12:11:42 +0000

    Changed the HTML of the page
    - The Menu now has position fixed.
    - The template has been removed.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,7 +6,7 @@ function Menu() {
     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" style={{backgroundColor: "#282c34"}}>
       <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 mx-5 mt-3">
         <li>
-            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none position-fixed">
               <img src="github-mark-white.svg" width="40px"></img>     
             </a>
         </li>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Menu from './components/Menu';
 import reportWebVitals from './reportWebVitals';
+import Profile from './components/Profile';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Menu />
+    <Profile />
     <App />
   </React.StrictMode>
 );
