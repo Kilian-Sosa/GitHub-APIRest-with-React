0602c661e5de087ae391d8e84c4e20704d2df667
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-03-13 13:25:06 +0000

    Squashed commit of the following:
    commit 1b7cbac6be5875202123798ff06886b322e186e8
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Mon Mar 13 13:24:15 2023 +0000
    
        feat: Added Autosuggest functionality
    
    commit e3b4856b4be04b87206d18b867bbeb0b3c9734be
    Author: NichelGomez <nichel8080@gmail.com>
    Date:   Tue Feb 28 22:50:16 2023 +0000
    
        No se cancelar esto
    
    commit c6497cb40446976dae41df1c39d267587fc6f1b3
    Author: NichelGomez <nichel8080@gmail.com>
    Date:   Tue Feb 28 22:44:28 2023 +0000
    
        Revert "fix: Minor changes"
    
        This reverts commit 8dc2e1b4e679fa06827a1c60f553d188a188fafe.
    
    commit e39b69b5ab47b3090225f5d4f43b5a36c04992d8
    Author: NichelGomez <nichel8080@gmail.com>
    Date:   Tue Feb 28 22:40:24 2023 +0000
    
        Update Profile.tsx
    
    commit 9098694c8ea37ce616a8f623dd4c339736d942ae
    Author: NichelGomez <nichel8080@gmail.com>
    Date:   Tue Feb 28 22:29:56 2023 +0000
    
        Axios y autosuggest instalado
    
    commit 8dc2e1b4e679fa06827a1c60f553d188a188fafe
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 16:17:15 2023 +0000
    
        fix: Minor changes
    
    commit 4ffe849ff6df3b27c4bc35cb10b6dd9d588723dc
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 16:04:02 2023 +0000
    
        feat: Now you can see the repositories of the user
    
        Modified some paddings and margins
    
    commit bbbd2604564604a2be4033ec6dfc5fbdc88ec42f
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 13:47:10 2023 +0000
    
        feat: Now Displays more information of the user
    
    commit f2a2d95b64e785465d838f8fdab47ea9c316b9bc
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 13:46:53 2023 +0000
    
        Modified some backgrounds
    
    commit 1bda7b28e3d4aaa1f8660d0770050e1d40190756
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 12:12:59 2023 +0000
    
        feat: 'Profile' component init
    
        Now accesses the GitHub API and gets the avatar of the User.
    
        Also setted a private token.
    
    commit b99763dfcdb731e5e97eb6d19f3780f9f5fbd0a2
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Tue Feb 28 12:11:42 2023 +0000
    
        Changed the HTML of the page
    
        - The Menu now has position fixed.
        - The template has been removed.
    
    commit edc1f881384d6b44c694c911474d2ce3ff3d27f9
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Fri Feb 24 13:44:04 2023 +0000
    
        Added new folder 'components'
    
    commit 6b58361668a33594b8bd557407705f5338f88b09
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Fri Feb 24 12:40:04 2023 +0000
    
        fix: Separated Menu in its own component
    
    commit 45c2020f2ff3628c3ba5f26c4c2e1c097c9a7587
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Fri Feb 24 12:34:52 2023 +0000
    
        feat: Added logo on the menu

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
-import React, { useState, ChangeEvent, MouseEventHandler } from "react";
+import React, { useState, ChangeEvent, MouseEventHandler, useEffect } from "react";
 import '../App.css';
 import options from '../token.js';
+import axios from 'axios';
+import Autosuggest, { SuggestionsFetchRequestedParams } from 'react-autosuggest';
 
 interface Repo {
   html_url: string,
@@ -10,54 +12,34 @@ interface Repo {
   forks: number
 }
 
+interface User {
+  login: string,
+  avatar_url: string,
+  name: string,
+  bio: string,
+  location: string,
+  email: string,
+  blog: string,
+  created_at: string,
+  public_repos: number,
+  public_gists: number,
+  followers: number,
+  following: number
+  html_url: string
+}
+
 function Profile() {
-  const [data, setData] = useState<Record<string, unknown>>({});
+  const [data, setData] = useState<User[]>([]);
+  const [user, setUser] = useState<User>({} as User);
   const [reposData, setRepos] = useState<Record<string, Repo>>({});
-  const [username, setUsername] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [bio, setBio] = useState<string>("");
-  const [location, setLocation] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const submitHandler: MouseEventHandler<HTMLButtonElement> = async e=>{
-    e.preventDefault();
-    const inputUserName = document.querySelector("input") as HTMLInputElement;
-    if(inputUserName.value.match("^\s*"), "") return;
-    setUsername(inputUserName.value);
-    
-    try{
-      const profile = await fetch(`https://api.github.com/users/${username}`, options);
-      const profileJson = await profile.json();
-      setData(profileJson);
-      console.log(profileJson);
-  
-      const imagen = document.getElementById("imagen") as HTMLImageElement;
-      if(profileJson["message"] !== "Not Found") {
-        imagen.src = profileJson.avatar_url;
-        setName(profileJson.name);
-        setBio(profileJson.bio);
-        setLocation(profileJson.location);
-        setEmail(profileJson.email);
-        setUsername("");
-        setRepos({});
-      }
-    }catch(err){
-      console.log(err);
-    }
-  }
+  const [userSelected, setUserSelected] = useState({});
+  const [value, setValue] = useState("");
+  const [flag, setFlag] = useState(false);
 
   const reposHandler: MouseEventHandler<HTMLButtonElement> = async e=>{
     e.preventDefault();
-    const inputUserName = document.querySelector("input") as HTMLInputElement;
-    if(inputUserName.value.match("^\s*"), "") return;
-    setUsername(inputUserName.value);
-    
     try{
-      const repos = await fetch(`https://api.github.com/users/${data["login"]}/repos`, options);
+      const repos = await fetch(`https://api.github.com/users/${user.login}/repos`, options);
       const reposJson = await repos.json();
       setRepos(reposJson);
       console.log(reposJson);
@@ -65,34 +47,102 @@ function Profile() {
       console.log(err);
     }
   }
+  
+  const onSuggestionsFetchRequested = ({ value }: SuggestionsFetchRequestedParams) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    if(inputLength < 3) return [];
+
+    axios.get(`https://api.github.com/search/users?q=${inputValue}%20in:login`, options).then(response => {
+      setData(response.data["items"]);
+    })
+  };
+
+  const onSuggestionsClearRequested = () => { 
+    setData([]);
+    setFlag(false);
+    setRepos({});
+    const imagen = document.getElementById("imagen") as HTMLImageElement;
+    imagen.src = "https://picsum.photos/300/300";
+  };
+
+  const getSuggestionValue = (suggestion: any) => { return suggestion.login; };
+
+  const renderSuggestion = (suggestion: any) => {
+    return (
+      <div onClick={()=>getUser(suggestion)}>
+        {suggestion.login}
+      </div>
+    );
+  };
+
+  const inputProps = {
+    placeholder: "Buscar usuario",
+    value,
+    onChange: (event: any, { newValue }: any) => {
+      setValue(newValue);
+    }
+  }
+
+  const eventEnter = (e: any)=>{
+    if(e.keyCode === 13){
+      let currentUser = data.filter((u => u.login === e.target.value.trim()));
+      console.log(currentUser);
+      getUser(currentUser[0]);
+    }
+  }
+
+  const getUser = (userA: any) => {
+    axios.get(userA.url, options).then(response => {
+      setUser(response.data);
+      setUserSelected(response.data);
+
+      if(user.avatar_url !== undefined){
+        setFlag(true);
+        const imagen = document.getElementById("imagen") as HTMLImageElement;
+        imagen.src = user.avatar_url;
+        setRepos({});
+      }  
+    }).catch(error => {
+      console.error(error);
+    });
+  }
 
   return (
     <div className="container-fluid">
-      <div className="d-flex justify-content-center gap-2 mb-5">
-        <input type="text" className="col-4" value={username} onChange={onChangeHandler}/>
-        <button type="button" className="btn btn-primary col-4" onClick={submitHandler}>Buscar</button>
+      <div className="d-flex justify-content-center gap-2 mb-5 text-white">
+        <Autosuggest 
+            suggestions={data}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+            onSuggestionSelected={eventEnter}
+          />
+        <button type="button" className="btn btn-primary col-4" id="btnSearch" onClick={()=>{getUser(userSelected);}}>Buscar</button>
       </div>
       <div className="col-12 d-flex justify-content-center align-items-center gap-2">
         <div className="col-3 card align-items-center">
           <img src="https://picsum.photos/300/300" alt="User Avatar" id="imagen" className="col-12" />
-          <p className="fs-4 card-title">{Object.keys(data).length > 0 ? (<a href={data["html_url"] + ""} target="_blank">{data["login"] + ""}</a>) : null}</p>
-          <p className="fs-6 card-title px-3">{bio}</p>
-          {Object.keys(data).length > 0 ? (<button type="button" className="btn btn-info col-8 text-white mb-2" onClick={reposHandler}>Mostrar Repositorios</button>) : null}
+          <p className="fs-4 card-title">{flag ? (<a href={user.html_url + ""} target="_blank">{user.login + ""}</a>) : null}</p>
+          <p className="fs-6 card-title px-3">{user.bio}</p>
+          {flag ? (<button type="button" className="btn btn-info col-8 text-white mb-2" onClick={reposHandler}>Mostrar Repositorios</button>) : null}
         </div>
-        {Object.keys(data).length > 0 ? (
+        {flag ? (
           <div className="col-5 card">
             <div className="text-white col-12">
-              <span className="bg-primary mx-1 col-2 rounded px-1">{data["public_repos"] + ""} Repositorios Públicos</span>
-              <span className="bg-success mx-1 col-2 rounded px-1">{data["public_gists"] + ""} Gists Públicos</span>
-              <span className="bg-info mx-1 col-2 rounded px-1">{data["followers"] + ""} Seguidores</span>
-              <span className="bg-warning mx-1 col-2 rounded px-1">{data["following"] + ""} Seguidos</span>
+              <span className="bg-primary mx-1 col-2 rounded px-1">{user.public_repos + ""} Repositorios Públicos</span>
+              <span className="bg-success mx-1 col-2 rounded px-1">{user.public_gists + ""} Gists Públicos</span>
+              <span className="bg-info mx-1 col-2 rounded px-1">{user.followers + ""} Seguidores</span>
+              <span className="bg-warning mx-1 col-2 rounded px-1">{user.following + ""} Seguidos</span>
             </div>
             <ul className="list-group">
-              <li className="list-group-item"><strong>Nombre: </strong>{name}</li>
-              <li className="list-group-item"><strong>Localización: </strong>{location}</li>
-              <li className="list-group-item"><strong>Correo Electrónico: </strong>{email}</li>
-              <li className="list-group-item"><strong>Página Web: </strong>{data["blog"] + ""}</li>
-              <li className="list-group-item"><strong>Se unió en: </strong>{data["created_at"] + ""}</li>
+              <li className="list-group-item"><strong>Nombre: </strong>{user.name}</li>
+              <li className="list-group-item"><strong>Localización: </strong>{user.location}</li>
+              <li className="list-group-item"><strong>Correo Electrónico: </strong>{user.email}</li>
+              <li className="list-group-item"><strong>Página Web: </strong>{user.blog + ""}</li>
+              <li className="list-group-item"><strong>Se unió en: </strong>{user.created_at + ""}</li>
             </ul>
         </div>
         ) : null}
